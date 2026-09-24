f76cecff9879d62238c6068e36a21c7870e8a776
seidasenmuhe2122 <seidasenmuhe2122@gmail.com>
2026-09-24 23:48:19 +0300

    update URLs

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,20 +1,22 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view),
+    path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
     path('home/', views.home_view, name='home'),
     path('jobs/', views.job_list, name='job_list'),
-    path('categories/', views.category_list_view, name='category_list'),
+    path('categories/', views.category_list_view, name='category_list.view'),
     path('blog/', views.blog_list_view, name='blog_list'),
     path('blog/<slug:slug>/', views.blog_detail_view, name='blog_detail'),
     path('job/<int:pk>/', views.job_detail, name='job_detail'),
-    path('advertisement/<int:pk>/impression.gif', views.advertisement_impression, name='advertisement_impression'),
+    path('advertisement/<int:pk>/impression.gif', views.advertisement_click, name='advertisement_impression'),
     path('advertisement/<int:pk>/click/', views.advertisement_click, name='advertisement_click'),
-    path('privacy-policy/', views.legal_page, {'page_type': 'privacy'}, name='privacy_policy'),
-    path('about-us/', views.legal_page, {'page_type': 'about'}, name='about_us'),
-    path('contact-us/', views.legal_page, {'page_type': 'contact'}, name='contact_us'),
+    path('privacy-policy/', views.legal_page, {'page_type': 'privacy_policy'}, name='privacy_policy'),
+    path('about-us/', views.legal_page, {'page_type': 'about_us'}, name='about_us'),
+    path('contact-us/', views.legal_page, {'page_type': 'contact_us'}, name='contact_us'),
     path('contact-us/message/', views.contact_message, name='contact_message'),
-    path('terms-and-conditions/', views.legal_page, {'page_type': 'terms'}, name='terms_and_conditions'),
+    path('terms-and-conditions/', views.legal_page, {'page_type': 'terms_and_conditions'}, name='terms_and_conditions'),
     path('pages/<slug:slug>/', views.custom_page, name='custom_page'),
 ]
