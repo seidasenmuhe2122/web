c03af8d24fdcf5fd34ab9330a858dc1467fab47b
seidasenmuhe2122 <seidasenmuhe2122@gmail.com>
2026-09-26 12:19:36 +0300

    Configure PostgreSQL database

--- jopportal/settings.py
+++ jopportal/settings.py
@@ -14,6 +14,7 @@ import os
 import re
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -231,11 +232,11 @@ WSGI_APPLICATION = 'jopportal.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'CONN_MAX_AGE': int(os.environ.get('DJANGO_DB_CONN_MAX_AGE', '60')),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=int(os.environ.get('DJANGO_DB_CONN_MAX_AGE', '60')),
+        conn_health_checks=True,
+    )
 }
 
 
